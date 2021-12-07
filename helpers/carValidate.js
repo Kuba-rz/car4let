ea9f6610e2cc55c241b58f447127182d522293fd
Jakub Rzeszutko <kubarzeszutko@gmail.com>
2021-12-07 20:06:15 +0100

    Polished the car validation and added generic seed

--- helpers/carValidate.js
+++ helpers/carValidate.js
@@ -3,21 +3,45 @@ const expressError = require('./expressError')
 
 function carValidate(req, res, next) {
     const schema = joi.object({
-        carMake: joi.string().required(),
-        carModel: joi.string().required(),
-        carYear: joi.number().required(),
-        carPrice: joi.number().required(),
-        carDescription: joi.string().required(),
-        carImages: joi.any().required()
+        carMake: joi.string().required().messages({
+            'string.base': `"Make" should be a type of 'text'`,
+            'string.empty': `"Make" cannot be an empty field`,
+            'string.required': `"Make" is a required field`
+        }),
+        carModel: joi.string().required().messages({
+            'string.base': `"Model" should be a type of 'text'`,
+            'string.empty': `"Model" cannot be an empty field`,
+            'string.required': `"Model" is a required field`
+        }),
+        carYear: joi.number().required().min(1900).max(2023).messages({
+            'number.base': `"Year" should be a type of 'number'`,
+            'number.empty': `"Year" cannot be an empty field`,
+            'number.min': `"Year" should have a minimum value of 1900`,
+            'number.max': `"Year" should have a maximum value of 2023`,
+            'any.required': `"Year" is a required field`
+        }),
+        carPrice: joi.number().required().min(0).messages({
+            'number.base': `"Price" should be a type of 'number'`,
+            'number.empty': `"Price" cannot be an empty field`,
+            'number.min': `"Price" should have a minimum value of 0`,
+            'any.required': `"Price" is a required field`
+        }),
+        carDescription: joi.string().required().messages({
+            'string.base': `"Description" should be a type of 'text'`,
+            'string.empty': `"Description" cannot be an empty field`,
+            'string.required': `"Description" is a required field`
+        }),
+        carImages: joi.any().required().messages({
+            'any.empty': `"Images" must be provided`,
+            'any.required': `"Images" must be provided`
+        })
     })
+
     const { carMake, carModel, carYear, carPrice, carDescription } = req.body
-    if (req.files && req.files.length) {
-        const images = req.files.map(f => ({ url: f.path, filename: f.filename }))
-    }
-    else {
-        throw new expressError('Please provide images', 400)
-    }
-    const { error } = schema.validate({ carMake, carModel, carYear, carPrice, carDescription, carImages: req.files })
+
+    const images = req.files.map(f => ({ url: f.path, filename: f.filename }))
+
+    const { error } = schema.validate({ carMake, carModel, carYear, carPrice, carDescription, carImages: images })
     if (error) {
         const msg = error.details.map(el => el.message).join(',')
         throw new expressError(msg, 400)
---
+++ helpers/seed.js
@@ -0,0 +1,35 @@
+const carModel = require('../models/carModel')
+const mongoose = require('mongoose')
+const carMakes = require('./carMakes')
+
+//Database URL depending if we are in production
+const DBUrl = process.env.mongoAtlasURL || 'mongodb://localhost:27017/car4let'
+
+//Connect the app to mongoose
+async function connectMongo() {
+    try {
+        await mongoose.connect(DBUrl);
+        console.log('All good and connected')
+    }
+    catch (err) {
+        console.log(`Error: ${err}`)
+    }
+}
+
+connectMongo()
+
+async function seed() {
+    await carModel.remove({})
+
+    for (let i = 0; i < 10; i++) {
+        const carMake = carMakes[[Math.floor(Math.random() * carMakes.length)]]
+        const carPrice = Math.floor(Math.random() * 100)
+        const carImages = { url: 'https://images.unsplash.com/photo-1568605117036-5fe5e7bab0b7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80', filename: 'SEEDING' }
+        const car = new carModel({ carMake, carModel: 'Seeding test', carYear: 2015, carPrice, carDescription: 'This is from a generic seed guys', carImages })
+        await car.save()
+    }
+}
+
+seed().then(() => {
+    mongoose.connection.close()
+})
